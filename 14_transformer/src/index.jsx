a8911b1d0f9b3ab2b7caae2781bbf4a9d94462b7
navib <ivan.baena.dev@gmail.com>
2018-01-31 20:24:21 -0500

    setup

---
+++ 14_transformer/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BabelTransformer from './components/transformer.jsx';
+
+
+ReactDOM.render(
+  <BabelTransformer />,
+  document.getElementById('app')
+)
