70432c497b9540bfac0d543696fa852b23e90c91
navib <ivan.baena.dev@gmail.com>
2018-01-31 21:28:01 -0500

    project pushes

---
+++ poke-list/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'whatwg-fetch';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    console.log(props);
+
+    this.state = {
+      pokemon: [],
+    };
+
+    this.loadPokemon = this.loadPokemon.bind(this);
+  }
+  loadPokemon(url) {
+    console.log("url: ", url);
+    fetch(url)
+      .then(response => {
+        return response.json();
+      }).then(json => {
+        console.log(json);
+        this.setState({
+          pokemon:json.results
+        })
+        console.log(this.state);
+      }).catch(err => {
+        console.log(err);
+      })
+  }
+  componentWillMount() {
+    this.loadPokemon(`${this.props.baseUrl}/pokemon/`);
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
