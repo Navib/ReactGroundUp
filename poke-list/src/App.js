ecd2d609f1d21bc65d7e3dad9a3ab02100fe8ca0
navib <ivan.baena.dev@gmail.com>
2018-02-01 20:21:54 -0500

    pagination working

--- poke-list/src/App.js
+++ poke-list/src/App.js
@@ -5,6 +5,7 @@ import 'whatwg-fetch';
 import PokeList from './components/PokeList';
 import { Col } from 'react-bootstrap';
 import ReactPaginate from 'react-paginate';
+import SelectItemsPerPageButtons from './components/SelectItemsPerPageButton'
 
 class App extends Component {
   constructor(props){
@@ -13,14 +14,16 @@ class App extends Component {
 
     this.state = {
       pokemon: [],
-      activePage: 0,
-      limit: 20,
+      activePage: 1,
+      limit: 50,
       offset: 0,
-      totalPages: 0
+      totalPages: 0,
+      count: 0
     };
 
     this.loadPokemon = this.loadPokemon.bind(this);
     this.handlePaginationSelect = this.handlePaginationSelect.bind(this);
+    this.handleLimitChange = this.handleLimitChange.bind(this);
   }
   loadPokemon(url) {
     fetch(url)
@@ -39,11 +42,19 @@ class App extends Component {
       })
   }
   handlePaginationSelect(selectedPage) {
-    console.log(selectedPage );
     let offset = this.state.limit * selectedPage.selected;
-    console.log(offset);
-
     this.loadPokemon(`${this.props.baseUrl}/pokemon/?limit=${this.state.limit}&offset=${offset}`);
+    this.setState({
+      activePage: selectedPage
+    })
+  }
+  handleLimitChange(event) {
+    this.setState({
+      limit: +event.target.innerHTML || this.state.count,
+      activePage: 1
+    }, () => {
+    this.loadPokemon(`${this.props.baseUrl}/pokemon/?limit=${this.state.limit}&offset=0`);
+    })
   }
   componentWillMount() {
     this.loadPokemon(`${this.props.baseUrl}/pokemon/`);
@@ -55,19 +66,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-
+        <SelectItemsPerPageButtons options={[10,50,100,200]} selectedValue={this.state.limit} allValue={this.state.count} onOptionSelected={this.handleLimitChange}/>
         <Col sm={8} md={10} smOffset={2} mdOffset={1}>
           <PokeList listofPokemon={this.state.pokemon} />
         </Col>
         <Col sm={9} smOffset={2}>
           <ReactPaginate
-            previousLabel={"previous"}
-            nextLabel={"next"}
+            previousLabel={""}
+            nextLabel={""}
             breakLabel={<a href="">...</a>}
             pageCount={this.state.totalPages}
             pageRangeDisplayed={2}
             marginPagesDisplayed={5}
             onPageChange={this.handlePaginationSelect}
+            forcePage={this.state.activePage - 1}
           />
         </Col>
       </div>
