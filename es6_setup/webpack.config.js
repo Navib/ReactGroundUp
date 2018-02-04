599338fc455215cf5fbbed559633fce3fedb8435
navib <ivan.baena.dev@gmail.com>
2018-02-03 19:31:44 -0500

    es6 redux code

---
+++ es6_setup/webpack.config.js
@@ -0,0 +1,16 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('src'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js[x]?$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ],
+  }
+}
